ffa0be7d4db096402a8d6794820448bbaa44a8a0
chihabMe <chihab.mg.me@gmail.com>
2025-04-08 19:25:25 +0100

    changed the number of gird cols in the home page

--- components/product-grid.tsx
+++ components/product-grid.tsx
@@ -22,7 +22,7 @@ export default function ProductGrid() {
       >
         Nouveauté
       </motion.h2>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.slice(0, visibleProducts).map((product, index) => (
           <ProductCard key={product.id} product={product} index={index} />
         ))}
